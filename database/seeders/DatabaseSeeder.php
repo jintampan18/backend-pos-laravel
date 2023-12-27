ad4c343dd8a0757d2ecf020a10d6769bc87a806a
jintampan18 <fauzipribadi18@gmail.com>
2023-12-28 00:14:36 +0700

    CRUD Users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // untuk memanggil fungsi UserSeeder
+        $this->call([
+            UserSeeder::class,
+        ]);
+
     }
 }
